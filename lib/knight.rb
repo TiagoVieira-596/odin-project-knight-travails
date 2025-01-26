c174924f5d6361c74f1d720cce3c5f45c25310f8
Tiago <tvhollowknight@gmail.com>
2025-01-26 15:54:12 -0300

    Finished the knight_moves method
    The method works as expected and returns the list of moves on the fastest route to the objective

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,5 +1,6 @@
+require 'pry-byebug'
 class Knight
-  def knight_moves(start, _goal)
+  def possible_moves(start)
     iteration = 0
     possible_moves = []
     x_moves = [2, 1, -1, -2, -2, -1, 1, 2]
@@ -16,7 +17,43 @@ class Knight
     end
     possible_moves
   end
+
+  def path_finder(start, goal)
+    makeable_moves = possible_moves(start)
+    all_moves = {}
+    all_moves[start] = 0
+    move_count = 1
+    until all_moves.key?(goal)
+      makeable_moves.each { |move| all_moves[move] = move_count if all_moves[move].nil? }
+      makeable_moves = []
+      all_moves.each_key do |move_made|
+        possible_moves(move_made).each do |move|
+          makeable_moves << move if all_moves[move].nil? || all_moves[move] > move_count
+        end
+      end
+      move_count += 1
+    end
+    all_moves
+  end
+
+  def knight_moves(start, goal)
+    all_paths = path_finder(start, goal)
+    puts "You've made it in #{all_paths[goal]} moves! Here's your path: "
+    reverse_path(goal, start, all_paths)
+    p goal
+  end
+
+  def reverse_path(goal, start, all_paths)
+    return start if start == goal
+
+    previous_moves = []
+    possible_moves(goal).each do |move|
+      previous_moves << move if !all_paths[move].nil? && all_paths[move] < all_paths[goal]
+    end
+    p reverse_path(previous_moves[0], start, all_paths)
+    goal
+  end
 end
 
 test = Knight.new
-p test.knight_moves([3, 3], [3, 3])
+test.knight_moves([0, 0], [7, 7])
